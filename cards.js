1945a669ce35d07107bf7c42f90f862d0eed264a
Vellyus <noisyboy026@gmail.com>
2023-01-23 16:15:07 +0100

    first commit

---
+++ cards.js
@@ -0,0 +1,47 @@
+const cards = [
+  {
+    number: 0,
+    name: "The Fool",
+    keywords: "Beginnings, innocence, spontaneity, a free spirit",
+    desc: "The Fool is numbered 0 – the number of unlimited potential – and so does not have a specific place in the sequence of the Tarot cards. The Fool can be placed either at the beginning of the Major Arcana or at the end. The Major Arcana is often considered The Fool’s journey through life and as such, he is ever present and therefore needs no number.",
+    img: "https://www.biddytarot.com/wp-content/uploads/2018/06/ET-MA-00-the-Fool.jpg"
+  },
+  {
+    number: 1,
+    name: "THe Magician",
+    keywords: "Manifestation, resourcefulness, power, inspired action",
+    desc: "The Magician card is numbered One – the number of new beginnings and opportunities – and associates with the planet of Mercury. He stands with one arm stretched upwards towards the Universe, and the other pointing down to the earth. His positioning represents his connection between the spiritual realms and the material realms. The Magician uses this relationship to create and manifest his goals in the physical realm. He is the conduit that converts energy into matter. The Magician’s robe is white, symbolising purity, and his cloak is red, representing worldly experience and knowledge.",
+    img: "https://www.biddytarot.com/wp-content/uploads/2018/06/ET-MA-01-Magician.jpg"
+  },
+  {
+    number: 2,
+    name: "THe High Priestess",
+    keywords: "Intuition, sacred knowledge, divine feminine, the subconscious mind",
+    desc: "The High Priestess sits in front of a thin veil decorated with pomegranates. The veil represents the separate conscious and subconscious realms, the seen and the unseen, and serves to keep casual onlookers out. Only the initiated may enter. The pomegranates on the veil are a symbol of abundance, fertility and the divine feminine, and are sacred to Persephone who ate a pomegranate seed in the underworld and was forced to return every year.",
+    img: "https://www.biddytarot.com/wp-content/uploads/2018/06/ET-MA-02-High-Priestess.jpg"
+  },
+  {
+    number: 3,
+    name: "The Empress",
+    keywords: "Femininity, beauty, nature, nurturing, abundance",
+    desc: "The Empress is a beautiful, full-figured woman with blonde hair and a peaceful aura about her. On her head, she wears a crown of twelve stars, showing her connection with the mystical realm and the cycles of the natural world (the twelve months of the year and the twelve planets). Her robe is patterned with pomegranates, symbolic of fertility, and sits upon a luxurious array of cushions and flowing red velvet. One cushion features the symbol of Venus, the planet of love, creativity, fertility, beauty and grace – the essence of The Empress.",
+    img: "https://www.biddytarot.com/wp-content/uploads/2018/06/ET-MA-03-The-Empress.jpg"
+  },
+  {
+    number: 4,
+    name: "The Emperor",
+    keywords: "Authority, establishment, structure, a father figure",
+    desc: "If The Empress is the Mother archetype of the Tarot deck, The Emperor is the Father. He sits upon a large stone throne, adorned with four rams’ heads (symbolic of his connection with Aries and the planet Mars). In his right hand, The Emperor holds an ankh, the Egyptian symbol of life, and in his left is an orb representing the world over which he rules.",
+    img: "https://www.biddytarot.com/wp-content/uploads/2018/06/ET-MA-04-Emperor.jpg"
+  },
+  {
+    number: 5,
+    name: "The Hierophant",
+    keywords: "Spiritual wisdom, religious beliefs, conformity, tradition,institutions",
+    desc: "The Hierophant is the masculine counterpart to The High Priestess. He is also known as the Pope or the Teacher in other Tarot decks and is ruled by Taurus.",
+    img: "https://www.biddytarot.com/wp-content/uploads/2018/04/MA-05-the-hierophant.png"
+  }
+
+]
+
+export default cards
