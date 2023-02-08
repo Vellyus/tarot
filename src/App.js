0e2d84c7cc15ff1dd0ec76c34ecc725459820f88
Vellyus <noisyboy026@gmail.com>
2023-02-08 19:26:22 +0100

    added Intro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css'
+import { Intro } from './Intro'
+
 
 function App() {
   return (
-    "meh"
+    <Intro />
   )
 }
 
---
+++ src/Intro.js
@@ -0,0 +1,8 @@
+export function Intro() {
+  return (
+    <div className="introDiv">
+      <p>Before you start the reading focus on one particular question. When you have the question in mind press the Start Button to start the reading.</p>
+      <button>Start</button>
+    </div>
+  )
+}
