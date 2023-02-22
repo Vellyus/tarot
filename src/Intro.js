f9cca9bf1dc0ca0c40a1b4dd816a9a2d4f2876dd
Vellyus <noisyboy026@gmail.com>
2023-02-22 10:11:17 +0100

    added handleStartButton function

--- src/Intro.js
+++ src/Intro.js
@@ -1,9 +1,17 @@
 export function Intro() {
+
+  const handleStartButton = () => {
+    const introDiv = document.querySelector(".introDiv")
+    const readingDiv = document.querySelector(".readingDiv")
+
+    introDiv.style.display = "none"
+    readingDiv.style.display = "block"
+  }
   return (
     <div className="introDiv">
       <h1>Tarot Reading</h1>
       <p>Before you start the reading focus on one particular question. When you have the question in mind press the Start Button to start the reading.</p>
-      <button>Start</button>
+      <button onClick={ () => handleStartButton() }>Start</button>
     </div>
   )
 }
