07cc193a003f37df94d6268f848443f5424db155
Vellyus <noisyboy026@gmail.com>
2023-03-13 23:53:37 +0100

    changed h1 in readingDiv

--- src/Reading.js
+++ src/Reading.js
@@ -3,7 +3,8 @@ import { Cards } from "./Cards"
 export function Reading() {
   return (
     <div className="readingDiv">
-      <h1>Click to reveal the cards</h1>
+      {/* <h1>Click to reveal the cards</h1> */ }
+      <h1>Click to reveal the card</h1>
       <div className="cardsDiv">
         <Cards />
       </div>
