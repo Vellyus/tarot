db38a6108fde6da18d23637edb1db175f3b401ba
Vellyus <noisyboy026@gmail.com>
2023-03-14 00:06:28 +0100

    changed heading in readingDiv

--- src/Reading.js
+++ src/Reading.js
@@ -4,7 +4,7 @@ export function Reading() {
   return (
     <div className="readingDiv">
       {/* <h1>Click to reveal the cards</h1> */ }
-      <h1>Click to reveal the card</h1>
+      <h2>Click to reveal the card</h2>
       <div className="cardsDiv">
         <Cards />
       </div>
