adf87986c130d51bec0c59e188e3b4964a039355
Vellyus <noisyboy026@gmail.com>
2023-04-10 15:57:31 +0200

    modified intro text

--- src/Intro.js
+++ src/Intro.js
@@ -48,7 +48,7 @@ export function Intro() {
   return (
     <div className="introDiv">
       <h1>Tarot Reading</h1>
-      <p>Before you start the reading focus on one particular question. When you have the question in mind press the Start Button to start the reading.</p>
+      <p>Before you start the reading focus on one particular question. When you have the question in mind press the Start Button to start the reading!</p>
       <button onClick={ () => handleStartButton() }>Start</button>
     </div>
   )
